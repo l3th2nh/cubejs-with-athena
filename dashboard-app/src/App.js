84b08fdbc9e1cbabc16f7d77b5a1c833835b1513
geonhui <kgh905@gmail.com>
2020-11-08 00:16:26 +0900

    init dashboard

---
+++ dashboard-app/src/App.js
@@ -0,0 +1,34 @@
+import './body.css';
+import 'antd/dist/antd.css';
+import React from 'react';
+import '@ant-design/compatible';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { Layout } from 'antd';
+import cubejs from '@cubejs-client/core';
+import { CubeProvider } from '@cubejs-client/react';
+import client from './graphql/client';
+import Header from './components/Header';
+const API_URL = "http://localhost:4000";
+const CUBEJS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MDQ3NDY0NDcsImV4cCI6MTYwNDgzMjg0N30.34krqe0XEn7uxyEVAGpEN6kyGS7xc_J9RkS3Y4lHZmA";
+const cubejsApi = cubejs(CUBEJS_TOKEN, {
+  apiUrl: `${API_URL}/cubejs-api/v1`
+});
+
+const AppLayout = ({
+  children
+}) => <Layout style={{
+  height: '100%'
+}}>
+    <Header />
+    <Layout.Content>{children}</Layout.Content>
+  </Layout>;
+
+const App = ({
+  children
+}) => <CubeProvider cubejsApi={cubejsApi}>
+    <ApolloProvider client={client}>
+      <AppLayout>{children}</AppLayout>
+    </ApolloProvider>
+  </CubeProvider>;
+
+export default App;
